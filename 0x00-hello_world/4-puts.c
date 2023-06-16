551b83792f710a5ae190ddd1dfc72b9c1f37a8a9
jgugwa <mirumbe.justus@gmail.com>
2023-06-16 08:10:48 -0400

    default

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*This is the main functio to excute the program*/int main(void)
-{char program[] = "\"Programming is like building a multilingual puzzle\n";
+/*This is the main function
+ * to excute the program
+ * Return:0 */int main(void)
+{char program[] = "\"Programming is like building a multilingual puzzle";
 puts(program);
 return (0);
 }
