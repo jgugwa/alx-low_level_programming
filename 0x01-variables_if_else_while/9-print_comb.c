e15c51487ce0c668930bfd83450de7231aa46a06
jgugwa <mirumbe.justus@gmail.com>
2023-06-19 14:57:46 -0400

    default

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/*
+*betty style doc for function main goes there */int main(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(i + '0');
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
+
