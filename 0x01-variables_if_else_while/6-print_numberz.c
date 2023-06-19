5078d58a5ca205b13cc42516be1c72d7a44a4ebc
jgugwa <mirumbe.justus@gmail.com>
2023-06-19 12:40:47 -0400

    default

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/*
+*main code goes here */int main(void)
+{
+char n = '0';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
+
