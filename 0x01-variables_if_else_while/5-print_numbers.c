b38b335cebdbd0486492f9878e78fa637f3b6dd3
jgugwa <mirumbe.justus@gmail.com>
2023-06-19 07:44:58 -0400

    default

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/*
+*betty style doc for function main goes there */int main(void)
+{
+for (int n = 0; n <= 9; n++)
+{
+printf("%d\n ", n);
+}
+return (0);
+}
+
+
