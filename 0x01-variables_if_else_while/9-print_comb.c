df3b78042f720d96606d6bb9d43fa28bab78402f
jgugwa <mirumbe.justus@gmail.com>
2023-06-19 15:30:57 -0400

    default

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,18 +3,19 @@
 
 /*
 *betty style doc for function main goes there */int main(void)
-{
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
+{int i = 0;
+while (i < 10)
 {
 putchar(i + '0');
-putchar(i + '0');
-putchar(',');
+if (i < 9)
+{
+putchar(44);
+putchar(32);
 }
+i++;
 }
 putchar('\n');
+
 return (0);
 }
 
