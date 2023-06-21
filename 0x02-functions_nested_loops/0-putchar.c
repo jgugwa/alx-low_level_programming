9fb4db167368ee418c5771284dfaf810861a6878
jgugwa <mirumbe.justus@gmail.com>
2023-06-22 00:56:32 +0300

    default

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 /*
  *main code here*/int main(void)
 {
-	char _putcher[] = "_putcher";
+	char putchar[] = "_putcher";
 	int i = 0;
 
-	while (_putcher[i] != '\0')
+	while (putchar[i] != '\0')
 	{
-		_putcher(_putcher[i]);
+		_putcher(putchar[i]);
 		i++;
 	}
 	_putchar('\n');
