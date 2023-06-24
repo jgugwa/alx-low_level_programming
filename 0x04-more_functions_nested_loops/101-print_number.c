8fa4a47e2f8d14529b51b30794cc4b186cdbaa79
jgugwa <mirumbe.justus@gmail.com>
2023-06-23 20:04:01 -0400

    default

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
 /**
- * print_number - Function that prints an integer.
- * @n: int type number
- * Description: Can only use _putchar to print.
- * do not use long
- */
-
+* print_number - Function that prints an integer.
+* @n: int type number
+* Description: Can only use _putchar to print.
+* do not use long
+*/
 /* introduce custome putcher function */
-void _putchar(char y) {
-    putchar(y);
+void _putchar(char y) 
+{putchar(y);
 }
-
 /* we basically have to apply the rucursive straytegy */
-void print_ind_digits(int n) {
-    if (n == 0) {
-        return (0);
-    }
-    print_ind_digits(n / 10);
-    _putchar('0' + (n % 10));
+void print_ind_digits(int n) 
+{ if (n == 0) 
+{
+return (0);
 }
-
+print_ind_digits(n / 10);
+_putchar('0' + (n % 10));
+}
 /*  define a function  to print the integer */
 void print_number(int n) {
-    /* Handle negative numbers */
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    /* zero to  be handled as a special case */
-    if (n == 0) {
-        _putchar('0');
-    }
-    else {
-        print_ind_digits(n);
-    }
+/* Handle negative numbers */
+if (n < 0) 
+{
+_putchar('-');
+n = -n;
+}
+/* zero to  be handled as a special case */
+if (n == 0) 
+{
+_putchar('0');
+}
+else 
+{
+print_ind_digits(n);
+}
 }
