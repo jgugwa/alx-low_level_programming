8837afcf6aa0914c244565eff4cbc77e726dc50a
jgugwa <mirumbe.justus@gmail.com>
2023-09-04 15:21:45 +0300

    default

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAINN_H
+#define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Brennan D Baraban
+ * File: main.h
+ * Auth: Justus Gugwa
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
+ *       written in the 0x15-file_io directory.
  */
 
 #include <sys/types.h>
@@ -17,4 +17,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
